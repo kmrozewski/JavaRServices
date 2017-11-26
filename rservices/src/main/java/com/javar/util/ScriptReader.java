7cf8a146e1e4fa4ca2e7403bad0212d670049c84
Konrad Mrozewski <k.mrozewski@kainos.com>
2017-11-27 00:19:02 +0100

    handle magic string

--- rservices/src/main/java/com/javar/util/ScriptReader.java
+++ rservices/src/main/java/com/javar/util/ScriptReader.java
@@ -6,12 +6,13 @@ import org.apache.commons.io.IOUtils;
 
 public final class ScriptReader {
 
+    private static final String ENCODING_UTF8 = "UTF8";
 
     private ScriptReader() {}
 
     public static String read(Class className, String fileName) {
         try {
-            return IOUtils.toString(className.getClassLoader().getResourceAsStream(fileName), "UTF8");
+            return IOUtils.toString(className.getClassLoader().getResourceAsStream(fileName), ENCODING_UTF8);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
