e4a3bbeb55a7f9508951345610579cc4db0dec98
Konrad Mrozewski <k.mrozewski@kainos.com>
2017-11-26 22:07:26 +0100

    add docker with java application, move dockers to separate folder

---
+++ ops-local/start-local-env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# build .jar with java application
+mvn clean package -f ../rservices/pom.xml
+
+# move .jar to appropriate docker folder
+cp ../rservices/target/rservices-*.jar ./rservices/rservices.jar
+
+# build and run docker-compose.yml
+docker-compose up --build
--- r-serve/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-R CMD Rserve.dbg --RS-conf /rserve.conf --vanilla --no-save
