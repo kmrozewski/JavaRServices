dcec3fc0f8fce045d2e915c0814af05f89cac37f
Konrad Mrozewski <k.mrozewski@kainos.com>
2018-03-22 22:10:16 +0100

    added anonymised model variables

--- rservices/src/main/java/com/javar/scoring/model3/Model3DataFrame.java
+++ rservices/src/main/java/com/javar/scoring/model3/Model3DataFrame.java
@@ -1,13 +1,206 @@
 package com.javar.scoring.model3;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.javar.scoring.models.ModelDataFrame;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Model3DataFrame extends ModelDataFrame {
 
+    @NotNull
+    @JsonProperty("X01")
+    private int inputVariable01;
 
+    @NotNull
+    @JsonProperty("X02")
+    private String inputVariable02;
+
+    @NotNull
+    @JsonProperty("X03")
+    private int inputVariable03;
+
+    @NotNull
+    @JsonProperty("X04")
+    private int inputVariable04;
+
+    @NotNull
+    @JsonProperty("X05")
+    private double inputVariable05;
+
+    @NotNull
+    @JsonProperty("X06")
+    private String inputVariable06;
+
+    @NotNull
+    @JsonProperty("X07")
+    private int inputVariable07;
+
+    @NotNull
+    @JsonProperty("X08")
+    private int inputVariable08;
+
+    @NotNull
+    @JsonProperty("X09")
+    private int inputVariable09;
+
+    @NotNull
+    @JsonProperty("X10")
+    private int inputVariable10;
+
+    @NotNull
+    @JsonProperty("X11")
+    private int inputVariable11;
+
+    @NotNull
+    @JsonProperty("X12")
+    private int inputVariable12;
+
+    @NotNull
+    @JsonProperty("X13")
+    private int inputVariable13;
+
+    @NotNull
+    @JsonProperty("X14")
+    private int inputVariable14;
+
+    @NotNull
+    @JsonProperty("X15")
+    private double inputVariable15;
+
+    @NotNull
+    @JsonProperty("X16")
+    private int inputVariable16;
+
+    @NotNull
+    @JsonProperty("X17")
+    private int inputVariable17;
+
+    @NotNull
+    @JsonProperty("X18")
+    private int inputVariable18;
+
+    @NotNull
+    @JsonProperty("X19")
+    private int inputVariable19;
+
+    @NotNull
+    @JsonProperty("X20")
+    private int inputVariable20;
+
+    @NotNull
+    @JsonProperty("X21")
+    private int inputVariable21;
+
+    @NotNull
+    @JsonProperty("X22")
+    private int inputVariable22;
+
+    @NotNull
+    @JsonProperty("X23")
+    private int inputVariable23;
+
+    @NotNull
+    @JsonProperty("X24")
+    private int inputVariable24;
+
+    @NotNull
+    @JsonProperty("X25")
+    private int inputVariable25;
+
+    @NotNull
+    @JsonProperty("X26")
+    private int inputVariable26;
+
+    @NotNull
+    @JsonProperty("X27")
+    private int inputVariable27;
+
+    @NotNull
+    @JsonProperty("X28")
+    private int inputVariable28;
+
+    @NotNull
+    @JsonProperty("X29")
+    private int inputVariable29;
+
+    @NotNull
+    @JsonProperty("X30")
+    private int inputVariable30;
+
+    @NotNull
+    @JsonProperty("X31")
+    private int inputVariable31;
+
+    @NotNull
+    @JsonProperty("X32")
+    private int inputVariable32;
+
+    @NotNull
+    @JsonProperty("X33")
+    private double inputVariable33;
+
+    @NotNull
+    @JsonProperty("X34")
+    private double inputVariable34;
+
+    @NotNull
+    @JsonProperty("X35")
+    private int inputVariable35;
+
+    @NotNull
+    @JsonProperty("X36")
+    private int inputVariable36;
+
+    @NotNull
+    @JsonProperty("X37")
+    private int inputVariable37;
+
+    @NotNull
+    @JsonProperty("X38")
+    private double inputVariable38;
+
+    @NotNull
+    @JsonProperty("X39")
+    private double inputVariable39;
+
+    @NotNull
+    @JsonProperty("X40")
+    private double inputVariable40;
+
+    @NotNull
+    @JsonProperty("X41")
+    private int inputVariable41;
+
+    @NotNull
+    @JsonProperty("X42")
+    private int inputVariable42;
+
+    @NotNull
+    @JsonProperty("X43")
+    private int inputVariable43;
+
+    @NotNull
+    @JsonProperty("X44")
+    private int inputVariable44;
+
+    @NotNull
+    @JsonProperty("X45")
+    private double inputVariable45;
+
+    @NotNull
+    @JsonProperty("X46")
+    private double inputVariable46;
+
+    @NotNull
+    @JsonProperty("X47")
+    private int inputVariable47;
+
+    @NotNull
+    @JsonProperty("X48")
+    private int inputVariable48;
 }
--- rservices/src/main/java/com/javar/scoring/model3/Model3Mappings.java
+++ rservices/src/main/java/com/javar/scoring/model3/Model3Mappings.java
@@ -1,5 +1,7 @@
 package com.javar.scoring.model3;
 
+import static com.javar.util.REXPParser.getREXP;
+
 import com.google.common.collect.ImmutableMap;
 
 final class Model3Mappings {
@@ -9,6 +11,54 @@ final class Model3Mappings {
 
     static ImmutableMap<String, Object> getMappings(Model3DataFrame modelDataFrame) {
         return ImmutableMap.<String, Object>builder()
+            .put("X01", getREXP(modelDataFrame::getInputVariable01))
+            .put("X02", getREXP(modelDataFrame::getInputVariable02))
+            .put("X03", getREXP(modelDataFrame::getInputVariable03))
+            .put("X04", getREXP(modelDataFrame::getInputVariable04))
+            .put("X05", getREXP(modelDataFrame::getInputVariable05))
+            .put("X06", getREXP(modelDataFrame::getInputVariable06))
+            .put("X07", getREXP(modelDataFrame::getInputVariable07))
+            .put("X08", getREXP(modelDataFrame::getInputVariable08))
+            .put("X09", getREXP(modelDataFrame::getInputVariable09))
+            .put("X10", getREXP(modelDataFrame::getInputVariable10))
+            .put("X11", getREXP(modelDataFrame::getInputVariable11))
+            .put("X12", getREXP(modelDataFrame::getInputVariable12))
+            .put("X13", getREXP(modelDataFrame::getInputVariable13))
+            .put("X14", getREXP(modelDataFrame::getInputVariable14))
+            .put("X15", getREXP(modelDataFrame::getInputVariable15))
+            .put("X16", getREXP(modelDataFrame::getInputVariable16))
+            .put("X17", getREXP(modelDataFrame::getInputVariable17))
+            .put("X18", getREXP(modelDataFrame::getInputVariable18))
+            .put("X19", getREXP(modelDataFrame::getInputVariable19))
+            .put("X20", getREXP(modelDataFrame::getInputVariable20))
+            .put("X21", getREXP(modelDataFrame::getInputVariable21))
+            .put("X22", getREXP(modelDataFrame::getInputVariable22))
+            .put("X23", getREXP(modelDataFrame::getInputVariable23))
+            .put("X24", getREXP(modelDataFrame::getInputVariable24))
+            .put("X25", getREXP(modelDataFrame::getInputVariable25))
+            .put("X26", getREXP(modelDataFrame::getInputVariable26))
+            .put("X27", getREXP(modelDataFrame::getInputVariable27))
+            .put("X28", getREXP(modelDataFrame::getInputVariable28))
+            .put("X29", getREXP(modelDataFrame::getInputVariable29))
+            .put("X30", getREXP(modelDataFrame::getInputVariable30))
+            .put("X31", getREXP(modelDataFrame::getInputVariable31))
+            .put("X32", getREXP(modelDataFrame::getInputVariable32))
+            .put("X33", getREXP(modelDataFrame::getInputVariable33))
+            .put("X34", getREXP(modelDataFrame::getInputVariable34))
+            .put("X35", getREXP(modelDataFrame::getInputVariable35))
+            .put("X36", getREXP(modelDataFrame::getInputVariable36))
+            .put("X37", getREXP(modelDataFrame::getInputVariable37))
+            .put("X38", getREXP(modelDataFrame::getInputVariable38))
+            .put("X39", getREXP(modelDataFrame::getInputVariable39))
+            .put("X40", getREXP(modelDataFrame::getInputVariable40))
+            .put("X41", getREXP(modelDataFrame::getInputVariable41))
+            .put("X42", getREXP(modelDataFrame::getInputVariable42))
+            .put("X43", getREXP(modelDataFrame::getInputVariable43))
+            .put("X44", getREXP(modelDataFrame::getInputVariable44))
+            .put("X45", getREXP(modelDataFrame::getInputVariable45))
+            .put("X46", getREXP(modelDataFrame::getInputVariable46))
+            .put("X47", getREXP(modelDataFrame::getInputVariable47))
+            .put("X48", getREXP(modelDataFrame::getInputVariable48))
             .build();
     }
 }
